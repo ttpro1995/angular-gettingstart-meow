49e6e8b8cb3ed84cadfdb129c8fba170dad1cdbe
ttpro1995 <ttpro1995@yahoo.com.vn>
2020-01-22 10:53:39 +0700

    fix route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -15,7 +15,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent }
+      { path: 'products/:productId', component: ProductDetailsComponent },
     ])
   ],
   declarations: [
--- src/app/products.ts
+++ src/app/products.ts
@@ -1,16 +1,19 @@
 export const products = [
   {
     name: 'Phone XL',
+    productId: 0,
     price: 799,
     description: 'A large phone with one of the best screens'
   },
   {
     name: 'Phone Mini',
+    productId: 1,
     price: 699,
     description: 'A great phone with one of the best cameras'
   },
   {
     name: 'Phone Standard',
+    productId: 2,
     price: 299,
     description: ''
   }
